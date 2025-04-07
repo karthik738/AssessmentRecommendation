361e109c95af82909584b1b61557774cc75440b9
K Srivatsa Karthikeya <KSrivatsa.Karthikeya@ivanti.com>
2025-04-07 05:30:46 +0530

    Req.txt added

--- backend/recommender.py
+++ backend/recommender.py
@@ -34,7 +34,7 @@ class SHLRecommender:
 
         self.llm_client = OpenAI(
             base_url="https://openrouter.ai/api/v1",
-            api_key=os.getenv("OPENROUTER_API_KEY"),
+            api_key=os.getenv("OPENROUTER_API_KEY")
         )
         self.llm_model = "anthropic/claude-3-haiku"
 
